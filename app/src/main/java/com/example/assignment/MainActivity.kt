ce79e83e42e2153e95d8016edf21a51125df4ea5
KelvinYou <ykekelvin0220@gmail.com>
2022-04-15 12:03:58 +0800

    nav update

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -24,7 +25,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 //        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 //        auth = Firebase.auth
+
+//        val homeFragment = HomeFragment()
+//        val questionFragment = QuestionFragment()
+//        val answerFragment = AnswerFragment()
+//        val profileFragment = ProfileFragment()
 //
+//        val navigation = intent.getIntExtra("navigation",0)
+//
+//        if(navigation == 0){
+//            makeCurrentFragment(homeFragment)
+//        }else {
+//            when(navigation){
+//                1 -> {
+//                    binding.bottomNavigation.selectedItemId = R.id.ic_home
+//                    makeCurrentFragment(homeFragment)
+//                }
+//                2 -> {
+//                    binding.bottomNavigation.selectedItemId = R.id.ic_question
+//                    makeCurrentFragment(questionFragment)
+//                }
+//                2 -> {
+//                    binding.bottomNavigation.selectedItemId = R.id.ic_answer
+//                    makeCurrentFragment(answerFragment)
+//                }
+//                4 -> {
+//                    binding.bottomNavigation.selectedItemId = R.id.ic_profile
+//                    makeCurrentFragment(profileFragment)
+//                }
+//            }
+//        }
+//
+//        binding.bottomNavigation.setOnItemSelectedListener {
+//            when(it.itemId){
+//                R.id.ic_home -> makeCurrentFragment(homeFragment)
+//                R.id.ic_question -> makeCurrentFragment(questionFragment)
+//                R.id.ic_answer -> makeCurrentFragment(answerFragment)
+//                R.id.ic_profile -> makeCurrentFragment(profileFragment)
+//            }
+//            true
+//        }
     }
 
 //    private fun makeCurrentFragment(fragment: Fragment) {
@@ -32,19 +72,5 @@ class MainActivity : AppCompatActivity() {
 //            replace(R.id.fragmentContainerView, fragment)
 //            commit()
 //        }
-//    }
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-//        val navController = findNavController(R.id.fragmentContainerView)
-//
-//        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.questionFragment, R.id.answerFragment, R.id.profileFragment))
-//
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        bottomNavigationView.setupWithNavController(navController)
-//
-//
 //    }
 }
