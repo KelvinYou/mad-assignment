9a6585305e830f4973ac9d4189f74b5d64d51d0c
KelvinYou <ykekelvin0220@gmail.com>
2022-04-16 20:27:15 +0800

    update

--- app/src/main/java/com/example/assignment/HomeFragment.kt
+++ app/src/main/java/com/example/assignment/HomeFragment.kt
@@ -8,13 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.example.assignment.data.TutorialAdapter
 import com.example.assignment.databinding.FragmentHomeBinding
 import com.example.assignment.models.TutorialViewModel
 import com.example.assignment.tutorial.*
 
 class HomeFragment : Fragment() {
-
     lateinit var binding: FragmentHomeBinding
     val tr : TutorialViewModel by activityViewModels()
 
@@ -44,7 +44,7 @@ class HomeFragment : Fragment() {
         }
 
         binding.btnAddTutorial.setOnClickListener{
-            val intent = Intent(activity, AddTutorialActivity::class.java)
+            val intent = Intent(activity, TutorialDetailActivity::class.java)
             startActivity(intent)
         }
 
@@ -65,3 +65,58 @@ class HomeFragment : Fragment() {
         }
     }
 }
+
+// ***** THE CODE WHAT I EXPECTED CAN USE WHILE CONNECTED TO FIRESTORE
+
+
+//class HomeFragment : Fragment() {
+//    lateinit var binding: FragmentHomeBinding
+//    val tr : TutorialViewModel by activityViewModels()
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home, container, false)
+//
+//        val recyclerView = binding.tutorialList
+//
+//        tr.getAll().observe(viewLifecycleOwner) { tutoriallist ->
+//
+//            var newList = tutoriallist.filter { a -> a.status == "Posted" }
+//
+//            val adapter = TutorialAdapter(newList)
+//            recyclerView.adapter = adapter
+//            adapter.setOnItemClickListener(object : TutorialAdapter.onItemClickedListener{
+//                override fun onItemClick(position: Int, id : String) {
+//                    val intent = Intent(activity, TutorialDetailActivity::class.java)
+//                        .putExtra("id", id)
+//                    startActivity(intent)
+//                }
+//            })
+//            recyclerView.setHasFixedSize(true)
+//        }
+//
+//        binding.btnAddTutorial.setOnClickListener{
+//            val intent = Intent(activity, TutorialDetailActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        binding.btnSortDate.setOnClickListener{ sort("createdDate") }
+//
+//        return binding.root
+//    }
+//
+//    private fun sort(field: String) {
+//        val reverse = tr.sort(field)
+//
+//        binding.btnSortDate.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+//
+//        val res = if (reverse) R.drawable.ic_down else R.drawable.ic_up
+//
+//        when (field) {
+//            "createdDate"    -> binding.btnSortDate.setCompoundDrawablesWithIntrinsicBounds(0, 0, res, 0)
+//        }
+//    }
+//}
--- app/src/main/java/com/example/assignment/tutorial/AddTutorialActivity.kt
+++ app/src/main/java/com/example/assignment/tutorial/AddTutorialActivity.kt
@@ -1,11 +1,11 @@
 package com.example.assignment.tutorial
 
-import android.content.ContentValues.TAG
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.helper.widget.MotionEffect.TAG
 import androidx.databinding.DataBindingUtil
 import com.example.assignment.R
 import com.example.assignment.data.Tutorial
@@ -13,16 +13,16 @@ import com.example.assignment.databinding.ActivityAddTutorialBinding
 import com.example.assignment.models.TutorialViewModel
 import com.example.assignment.models.cropToBlob
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.Blob
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.util.*
 
+
 class AddTutorialActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddTutorialBinding
-//    val tr = TutorialViewModel()
-
-
+    val tr = TutorialViewModel()
 
     private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == RESULT_OK) {
@@ -53,70 +53,32 @@ class AddTutorialActivity : AppCompatActivity() {
    }
 
    private fun post() {
-       val db = Firebase.firestore
+       val database = FirebaseDatabase.getInstance().getReference("Tutorials")
 
-       // Create a new user with a first and last name
-       val user = hashMapOf(
-           "first" to "Ada",
-           "last" to "Lovelace",
-           "born" to 1815
-       )
+       val tutorialTitle  = binding.edtTutorialTitle.text.toString().trim()
+       val tutorialDetail = binding.edtTutorialDetail.text.toString()
+       val currentDate = Date()
 
-// Add a new document with a generated ID
-       db.collection("users")
-           .add(user)
-           .addOnSuccessListener { documentReference ->
-               Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-           }
-           .addOnFailureListener { e ->
-               Log.w(TAG, "Error adding document", e)
-           }
-//       val tutorialTitle  = binding.edtTutorialTitle.text.toString().trim()
-//       val tutorialDetail = binding.edtTutorialDetail.text.toString()
-//       val currentDate = Date()
-//
-//       if(tutorialTitle == ""){
-//           binding.edtTutorialTitle.error = "Please enter a title"
-//           binding.edtTutorialTitle.requestFocus()
-//           return
-//       }
-//
-//       else if (tutorialDetail == ""){
-//           binding.edtTutorialDetail.error = "Please input content"
-//           binding.edtTutorialDetail.requestFocus()
-//           return
-//       }
-//
-//       else if (wordCountCheck(tutorialTitle)) {
-//           binding.edtTutorialTitle.error = "Your title is more than 15 words"
-//           binding.edtTutorialTitle.requestFocus()
-//           return
-//       }
-//
-//
-//       val tutorial = Tutorial (
-//           title = tutorialTitle,
-//           content = tutorialDetail,
-//           status = "Posted",
-//           createdDate = currentDate,
-//           modifiedDate = currentDate,
-//           ownerID = Firebase.auth.currentUser?.email ?: "",
-//           Image =
-//           try {
-//               binding.imgAddTutorialImage.cropToBlob(200,200)
-//           }catch (e : Exception){
-//               Blob.fromBytes(ByteArray(0))
-//           }
-//
-//       )
-//
-//       tr.insert(tutorial)
+       if(tutorialTitle == ""){
+           binding.edtTutorialTitle.error = "Please enter a title"
+           binding.edtTutorialTitle.requestFocus()
+           return
+       }
 
-//        KToasty.success(this,"Tutorial Added Successfully.", Toast.LENGTH_LONG).show()
+       else if (tutorialDetail == ""){
+           binding.edtTutorialDetail.error = "Please input content"
+           binding.edtTutorialDetail.requestFocus()
+           return
+       }
 
-//       finish()
-//
-//       reset()
+       else if (wordCountCheck(tutorialTitle)) {
+           binding.edtTutorialTitle.error = "Your title is more than 15 words"
+           binding.edtTutorialTitle.requestFocus()
+           return
+       }
+
+       database.child("T001").setValue(Tutorial("T001", tutorialTitle, tutorialDetail,
+           "Posted", currentDate, currentDate, Firebase.auth.currentUser?.email ?: ""))
 
    }
 
