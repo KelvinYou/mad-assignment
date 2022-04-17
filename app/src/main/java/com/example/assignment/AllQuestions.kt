77436264b1104cd2ca49449b026f8d29b8635198
YapHongKiat <yaphk-wm19@student.tarc.edu.my>
2022-04-17 19:06:37 +0800

    pull new

---
+++ app/src/main/java/com/example/assignment/AllQuestions.kt
@@ -0,0 +1,71 @@
+package com.example.assignment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.assignment.AnswerQuestions
+import com.example.assignment.Questions
+import com.example.assignment.databinding.FragmentHomeBinding
+import com.example.assignment.models.TutorialViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+
+class AllQuestions : AppCompatActivity() {
+    lateinit var binding: FragmentHomeBinding
+    val tr : TutorialViewModel by activityViewModels()
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var database : DatabaseReference
+    private lateinit var quesRecycleView: RecyclerView
+    private lateinit var quesArrayList: ArrayList<Questions>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions_list)
+
+        quesRecycleView = findViewById(R.id.questionsList)
+        quesRecycleView.layoutManager = LinearLayoutManager(this)
+        quesRecycleView.setHasFixedSize(true)
+
+        quesArrayList = arrayListOf<Questions>()
+        getQuesData()
+    }
+
+    private fun getQuesData() {
+
+        database = FirebaseDatabase.getInstance().getReference("Questions")
+
+        database.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for(quesSnapshot in snapshot.children){
+
+                        val ques = quesSnapshot.getValue(Questions::class.java)
+                        quesArrayList.add(ques!!)
+                    }
+
+                    var adapter = QuestionListAdapter(quesArrayList)
+                    quesRecycleView.adapter = adapter
+                    adapter.setOnClickListener(object : QuestionListAdapter.onItemClickListener{
+                        override fun onItemClick(position: Int) {
+                            val intent = Intent(this@AllQuestions, AnswerQuestions::class.java)
+                            this@AllQuestions.startActivity(intent)
+                        }
+
+                    })
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
+
+    }
+}
--- app/src/main/java/com/example/assignment/Comment.kt
+++ app/src/main/java/com/example/assignment/Comment.kt
@@ -1,10 +1,27 @@
 package com.example.assignment
 
+/*data class Comment(
+    var ansQuestionTitle: String ? = null,
+    var ansComment: String ? = null,
+    var ansDate: String ? = null,
+    var ansIdName: String ? = null
+)*/
+
 class Comment {
-    var ansQuestionTitle = ""
+    /*var ansQuestionTitle = ""
     var ansComment = ""
     var ansDate = ""
-    var ansIdName = ""
+    var ansIdName = ""*/
+
+    var ansQuestionTitle: String ? = null
+    var ansComment: String ? = null
+    var ansDate: String ? = null
+    var ansIdName: String ? = null
+
+    constructor(){
+
+    }
+
     constructor(ansQuestionTitle:String,ansComment:String, ansDate:String, ansIdName:String){
         this.ansQuestionTitle = ansQuestionTitle
         this.ansComment = ansComment
